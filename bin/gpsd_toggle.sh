286216cc7a43826b3b3a16d9944496f44d2edcb2
Taylor Chittick <taylor@laylineautomation.com>
2015-06-19 20:38:56 +0000

    update shell

--- bin/gpsd_toggle.sh
+++ bin/gpsd_toggle.sh
@@ -4,6 +4,10 @@
 case "$(pidof gpsd | wc -w)" in
 	0) echo "GPSD not running. Starting Service."
 		sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
+		while ! pidof gpsd >> /dev/null ;
+		do
+			sleep 1
+		done
 		;;
 	1) echo "Killing GPSD process(s)."
 		sudo killall gpsd
